a2cc741caa782245f697af37d9ff0a21c12ee95e
Ragnar Cleveman <rawegenar@gmail.com>
2022-09-27 14:43:06 +0200

    Add project files.

---
+++ TcpApp.Client/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Net.Sockets;
+using System.Text;
+
+using var tcpClient = new TcpClient();
+await tcpClient.ConnectAsync("localhost", 5005);
+
+using var stream = tcpClient.GetStream();
+
+using var reader = new StreamReader(stream, new UTF8Encoding(false));
+using var writer = new StreamWriter(stream, new UTF8Encoding(false));
+
+
+while (tcpClient.Connected)
+{
+    Console.Write("Input your message: ");
+    var input = Console.ReadLine();
+
+    await writer.WriteAsync(input);
+    await writer.FlushAsync();
+
+    //Thread.Sleep(1000);
+
+    while (!reader.EndOfStream)
+    {
+        var response = await reader.ReadToEndAsync();
+
+        Console.WriteLine(response);
+        break;
+    }
+
+}
+
+// writer.FlushAsync
+
---
+++ TcpApp.Server/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+var endpoint = new IPEndPoint(IPAddress.Loopback, 5005);
+
+var listener = new TcpListener(endpoint);
+
+listener.Start();
+
+Console.WriteLine("Listening");
+
+using var tcpClient = listener.AcceptTcpClient();
+
+Console.WriteLine("Accepted connection");
+
+while (true)
+{
+
+    var stream = tcpClient.GetStream();
+    var client = tcpClient.Client;
+
+    //Request part
+    var buffer = new byte[2048];
+    var lengthReceived = await client.ReceiveAsync(buffer, SocketFlags.None);
+
+    var data = buffer.Take(lengthReceived).ToArray();
+
+    Console.WriteLine($"Received: {Encoding.UTF8.GetString(data)}");
+
+    //Thread.Sleep(1000);
+    //Response part
+
+    var response = $"Server received: {Encoding.UTF8.GetString(data)}";
+
+
+    Console.WriteLine("Sending response..");
+    await writer.WriteAsync(response);
+    await writer.FlushAsync();
+    Console.WriteLine($"Send complete.");
+}
+Console.WriteLine("Connection terminated.");
